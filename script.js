cc82370be27f58db96c3e05456f3e2df40f5e218
test <test>
2021-11-08 17:03:11 +0500

    js

---
+++ script.js
@@ -0,0 +1,38 @@
+var a = 1
+var b = 2
+var c = 3
+var str = prompt('Ismingizni kiriting')
+console.log('Sizning ismingiz: ' +  str);
+var num = +prompt('Sizning yoshingiz');
+console.log ( 'Sizning yoshingiz: ' + num);
+
+var str = prompt('Misollarni yeching')
+var num = + prompt('2 + 3 = ?')
+console.log('1-misol:' +'2 + 3 = ?' +'javob: '+ 5 + ' Sizning javob:' + num);
+var num = + prompt('3 + 3 = ?')
+console.log('2-misol: '+ '3 + 3 = ?'+ 'javob: '+ 6 +' Sizning javob:' + num);
+
+var num = + prompt('2 - 3 = ?')
+console.log('3-misol:'+ '2 - 3 = ?'+'javob:'+ 1 +'Sizning javob:'+ num);
+
+var num = + prompt('2 * 3 = ?')
+console.log('4-misol: '+'2 * 3 = ?'+'javob:'+ 6+'Sizning javob:'+ num);
+
+var num = + prompt('9 / 3 = ?')
+console.log('5-misol'+ '9 / 3 = ?'+' javob:'+ 3+'Sizning javob:'+ num);
+
+var num = + prompt('20 % 6 = ?')
+console.log('6-misol'+'20 % 6 = ?'+'javob:'+ 30+'Sizning javob:'+ num);
+
+var num = + prompt('14 * 3 = ?')
+console.log('7-misol'+'14 * 3 = ?'+'javob:'+ 42+'Sizning javob:'+ num);
+
+var num = + prompt('954 / 3 = ?')
+console.log('8-misol'+'954 / 3 = ?'+'javob:'+ 318+'Sizning javob:'+ num);
+var num = + prompt('545 / 6 = ?')
+console.log('9-misol '+' 545 / 6 = ?'+'javob: '+ 90.83+'Sizning javob:'+ num);
+var num = + prompt('35345 / 455 = ?')
+console.log('10-misol '+'35345 / 455 = ?'+'javob: '+ 77.6813+'Sizning javob:'+ num);
+var str = prompt('Natijalarni korish uchun F 12 ni bosing ' )
+
+
